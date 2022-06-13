3d4a41174f2377bec12eed79722b476cb03555a9
Maciej Malycha <koadjunky@gmail.com>
2022-06-13 12:03:52 +0200

    Get all tasks

--- src/clickup-automation/clickup_daily/clickup.py
+++ src/clickup-automation/clickup_daily/clickup.py
@@ -20,6 +20,7 @@ def clickup_query(path : str) -> dict:
     return response.json()
 
 
+# TODO: Error handling
 def get_teams() -> List[str]:
     data = clickup_query("/team")
     return [team["id"] for team in data["teams"]]
@@ -33,6 +34,39 @@ def get_spaces(team_ids):
     return space_ids
 
 
+def get_folders(space_ids):
+    folder_ids = []
+    for space_id in space_ids:
+        data = clickup_query(f"/space/{space_id}/folder?archived=false")
+        folder_ids.extend([folder["id"] for folder in data["folders"]])
+    return folder_ids
+
+
+def get_lists(space_ids, folder_ids):
+    list_ids = []
+    for space_id in space_ids:
+        data = clickup_query(f"/space/{space_id}/list?archived=false")
+        list_ids.extend([list["id"] for list in data["lists"]])
+    for folder_id in folder_ids:
+        data = clickup_query(f"/folder/{folder_id}/list?archived=false")
+        list_ids.extend([list["id"] for list in data["lists"]])
+    return list_ids
+
+
+# TODO: Pagination
+def get_tasks(list_ids):
+    tasks = []
+    for list_id in list_ids:
+        data = clickup_query(f"/list/{list_id}/task?archived=false&subtasks=true")
+        if len(data["tasks"]) > 99:
+            print("pagination needed")
+        tasks.extend(data["tasks"])
+    return tasks
+
+
 if __name__ == '__main__':
     team_ids = get_teams()
-    print(get_spaces(team_ids))
+    space_ids = get_spaces(team_ids)
+    folder_ids = get_folders(space_ids)
+    list_ids = get_lists(space_ids, folder_ids)
+    print(get_tasks(list_ids))
